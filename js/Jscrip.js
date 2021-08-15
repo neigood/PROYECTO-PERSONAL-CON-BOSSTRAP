abfd0914c7f1bc2d55a231b788de13a756f02c95
neigood <neifergonzalez@gmail.com>
2021-08-15 12:22:58 -0400

    VERSION 2.0
    añadiendo el color aleatorio al fondo del texto

--- js/Jscrip.js
+++ js/Jscrip.js
@@ -27,3 +27,29 @@ window.addEventListener('load', function(){
 window.addEventListener('scroll', function(){
     menu();
 });
+
+//codigo de cambio de color de fondo//
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("hexadecimal").innerHTML = color;
+	document.getElementById("text").innerHTML = "Copiar Color";
+}
+
+function copiarAlPortapapeles(hexadecimal) {
+	var aux = document.createElement("input");
+	aux.setAttribute("value", document.getElementById(hexadecimal).innerHTML);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+	document.getElementById("text").innerHTML = "Copiado!";
+}
