9cd2d30758a68a84638eb9232e2ee7d864ad4b06
neigood <neifergonzalez@gmail.com>
2021-08-18 21:30:58 -0400

    version mapa
    agregando mapa al contacto

--- js/Jscrip.js
+++ js/Jscrip.js
@@ -53,3 +53,15 @@ function copiarAlPortapapeles(hexadecimal) {
 	document.body.removeChild(aux);
 	document.getElementById("text").innerHTML = "Copiado!";
 }
+
+function iniciarMap(){
+    var coord = {lat:-33.4488897 ,lng: -70.6692655};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
